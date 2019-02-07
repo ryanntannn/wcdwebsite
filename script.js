023c4ba6c550720a6a6cd85be08b52888463de41
tanryan777 <tanryan777@gmail.com>
2019-02-07 23:25:46 +0800

    kaleido, firstcrank

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
+
   $("header nav a").hover(function(){
     $(this).css("background-color", "orange");
+    $(this).css("color", "#000000");
     }, function(){
     $(this).css("background-color", "");
+    $(this).css("color", "#ffffff");
   });
+
+  $("article.kaliedo a").hover(function(){
+    //TODO
+  });
+
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 300) {
+	    $("header").css("background" , "#121212");
+      $("article.kaliedo").css("clip-path", "polygon(100% 0, 100% 91%, 60% 100%, 0 92%, 0 0)");
+	  }
+
+	  else{
+		  $("header").css("background" , "none");
+      $("article.kaliedo").css("clip-path", "polygon(100% 0, 100% 100%, 30% 100%, 0 100%, 0 0)");
+	  }
+  })
 });
