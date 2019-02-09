6b61f62d5a56eed9956b199e8e31c3234e47d3e6
tanryan777 <tanryan777@gmail.com>
2019-02-10 01:30:11 +0800

    threerows and animations

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//p1829105 Ryan Tan Rui Ann
 $(document).ready(function(){
 
   $("header nav a").hover(function(){
@@ -9,19 +10,30 @@ $(document).ready(function(){
   });
 
   $("article.kaliedo a").hover(function(){
+    $("article.kaliedo img").css("top", "37%");
+    $("article.kaliedo").css("background-position", "bottom");
+    }, function(){
+    $("article.kaliedo img").css("top", "40%");
+    $("article.kaliedo").css("background-position", "center");
+  });
+
+  $("article.kaliedo a").hover(function(){
     //TODO
   });
 
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
-	  if (scroll > 300) {
+
+	  if (scroll > 300) { //scrolled down from the top
 	    $("header").css("background" , "#121212");
       $("article.kaliedo").css("clip-path", "polygon(100% 0, 100% 91%, 60% 100%, 0 92%, 0 0)");
+      $("article.firstcrank").css("top", "100%");
 	  }
 
-	  else{
+	  else{ //still at the top
 		  $("header").css("background" , "none");
       $("article.kaliedo").css("clip-path", "polygon(100% 0, 100% 100%, 30% 100%, 0 100%, 0 0)");
+      $("article.firstcrank").css("top", "20%");
 	  }
   })
 });
